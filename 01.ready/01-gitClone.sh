12c42772ad7c367bc3b390c15f44e8ebd2347d47
icanhereyou <1336746873@qq.com>
2021-06-30 15:05:09 +0800

    add git_config_user

--- 01.ready/01-gitClone.sh
+++ 01.ready/01-gitClone.sh
@@ -5,6 +5,9 @@ apt install -y git
 
 # sh
 git config --global credential.helper store
+git config --global user.name icanhereyou
+git config --global user.email 1336746873@qq.com
+git config --global user.password "+hb@4EK*&E6Q"
 
 # 克隆项目
 git clone https://github.com/icanhereyou/debian-ssh.git
