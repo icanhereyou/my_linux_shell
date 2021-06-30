f765dd09839c7eb3b86811ed09f1559f08046c46
icanhereyou <1336746873@qq.com>
2021-06-30 14:59:43 +0800

    git_store_password

--- 01.ready/01-gitClone.sh
+++ 01.ready/01-gitClone.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # 安装git
 apt install -y git
-# 克隆项目
-git clone https://github.com/icanhereyou/debianssh.git
 
+# sh
+git config --global credential.helper store
+
+# 克隆项目
+git clone https://github.com/icanhereyou/debian-ssh.git
+icanhereyou
++hb@4EK*&E6Q
